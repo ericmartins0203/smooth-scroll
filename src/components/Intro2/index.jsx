e3822d5e13e6dffdec24a0564538d4c74f75eaad
ericmartins0203 <estevesmartinseric@gmail.com>
2023-10-10 22:28:50 -0300

    build

--- src/components/Intro2/index.jsx
+++ src/components/Intro2/index.jsx
@@ -1,29 +1,17 @@
-import React, { useLayoutEffect, useRef } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import styles from './style.module.css';
 import Image from 'next/image';
 import gsap from 'gsap';
 import { motion } from 'framer-motion';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Centered from '../PixelTransition/Centered/Index';
 
-
-const anim = {
-  initial: {
-    opacity: 0
-  },
-  open: (i) => ({
-    opacity: 1,
-    transition: {duration: 0, delay: 0.03 * i}
-  }),
-  closed: (i) => ({
-    opacity: 0,
-    transition: {duration: 0, delay: 0.03 * i}
-  })
-}
 
 export default function Index({menuIsActive}) {
 
   const background = useRef(null);
   const introImage = useRef(null);
+  
 
   useLayoutEffect( () => {
     gsap.registerPlugin(ScrollTrigger);
@@ -41,53 +29,9 @@ export default function Index({menuIsActive}) {
       .to(introImage.current, {height: "200px"}, 0)
   }, [])
 
-  /**
-   * Shuffles array in place (Fisher–Yates shuffle).
-   * @param {Array} a items An array containing the items.
-   */
-  const shuffle = (a) => {
-    let j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = a[i];
-      a[i] = a[j];
-      a[j] = x;
-    }
-    return a;
-  }
-
-  const getBlocks = () => {
-    const { innerWidth, innerHeight } = window;
-    const blockSize = innerWidth * 0.05;
-    const nbOfBlocks = Math.ceil(innerHeight / blockSize);
-    const shuffledIndexes = shuffle([...Array(nbOfBlocks)].map( (_, i) => i))
-    return shuffledIndexes.map( (randomIndex, index) => {
-      return (
-        <motion.div 
-          key={index} 
-          className={styles.block}
-          variants={anim}
-          initial="initial"
-          animate={menuIsActive ? "open" : "closed"}
-          custom={randomIndex}
-        />
-      )
-    })
-  }
-
   return (
-    <>
-      <div className={styles.pixelBackground}>
-        {
-          [...Array(20)].map( (_, index) => {
-            return <div key={index} className={styles.column}>
-              {
-                (typeof window !== 'undefined') && getBlocks()
-              }
-            </div>
-          })
-        }
-      </div>
+    <div className={styles.container}>
+      <Centered menuIsActive={menuIsActive}/>
       <div className={styles.intro}>
 
         <div className={styles.backgroundImage} ref={background}>
@@ -111,6 +55,6 @@ export default function Index({menuIsActive}) {
           <h1 data-scroll data-scroll-speed="0.7">SMOOTH SCROLL</h1>
         </div>
       </div>
-    </>
+    </div>
   )
 }
