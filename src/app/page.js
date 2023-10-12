38f40ecbfa8ee738eb6793683a757e4838d992e6
ericmartins0203 <estevesmartinseric@gmail.com>
2023-10-11 22:09:25 -0300

    preloader

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,14 @@ import styles from './page.module.css'
 import Intro from '../components/Intro2';
 import Description from '../components/Description';
 import Projects from '../components/Projects';
+import Preloader from '../components/Preloader';
 import Header from '../components/Header';
 import Menu from '../components/Menu';
+import { AnimatePresence } from 'framer-motion';
 
 export default function Home() {
 
+  const [isLoading, setIsLoading] = useState(true);
   const [menuIsActive, setMenuIsActive] = useState(false);
 
   useEffect( () => {
@@ -17,6 +20,12 @@ export default function Home() {
       async () => {
         const LocomotiveScroll = (await import('locomotive-scroll')).default;
         const locomotiveScroll = new LocomotiveScroll();
+
+        setTimeout( () => {
+          setIsLoading(false);
+          document.body.style.cursor = 'default'
+          window.scrollTo(0,0);
+        }, 2000)
       }
     )()
 
@@ -24,6 +33,9 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
+      <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
       <Header menuIsActive={menuIsActive} setMenuIsActive={setMenuIsActive}/>
       <Menu menuIsActive={menuIsActive}/>
       <Intro menuIsActive={menuIsActive}/>
